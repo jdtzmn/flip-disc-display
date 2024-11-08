5d731cea791a5427629ee54991a24bc780c8170f
Jacob Daitzman <jdtzmn@gmail.com>
2024-11-08 08:22:22 -0500

    Get magnet to flip using pwm

--- src/main.c
+++ src/main.c
@@ -4,17 +4,27 @@
  * SPDX-License-Identifier: CC0-1.0
  */
 
+#include <math.h>
 #include <stdio.h>
 
+#include "driver/ledc.h"
 #include "esp_chip_info.h"
+#include "esp_err.h"
 #include "esp_flash.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "sdkconfig.h"
 
-void app_main(void) {
-  printf("Hello world!\n");
+#define LEDC_TIMER LEDC_TIMER_0
+#define LEDC_MODE LEDC_LOW_SPEED_MODE
+#define LEDC_OUTPUT_PIN GPIO_NUM_18
+#define LEDC_CHANNEL LEDC_CHANNEL_0
+#define LEDC_DUTY_RES LEDC_TIMER_10_BIT
+#define LEDC_DUTY \
+  (pow(2, LEDC_DUTY_RES) * 0.8)  // Set duty percent. (2 ** 13) * %
+#define LEDC_FREQUENCY (20000)   // Set frequency to 20 kHz
 
+static void print_chip_info(void) {
   /* Print chip information */
   esp_chip_info_t chip_info;
   uint32_t flash_size;
@@ -37,12 +47,53 @@ void app_main(void) {
 
   printf("Minimum free heap size: %d bytes\n",
          esp_get_minimum_free_heap_size());
+}
 
-  for (int i = 10; i >= 0; i--) {
-    printf("Restarting in %d seconds...\n", i);
+static void ledc_init(void) {
+  // Prepare and then apply the LEDC PWM timer configuration
+  ledc_timer_config_t ledc_timer = {.speed_mode = LEDC_MODE,
+                                    .duty_resolution = LEDC_DUTY_RES,
+                                    .timer_num = LEDC_TIMER,
+                                    .freq_hz = LEDC_FREQUENCY,
+                                    .clk_cfg = LEDC_AUTO_CLK};
+  ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
+
+  // Prepare and then apply the LEDC PWM channel configuration
+  ledc_channel_config_t ledc_channel = {.speed_mode = LEDC_MODE,
+                                        .channel = LEDC_CHANNEL,
+                                        .timer_sel = LEDC_TIMER,
+                                        .intr_type = LEDC_INTR_DISABLE,
+                                        .gpio_num = LEDC_OUTPUT_PIN,
+                                        .duty = 0,
+                                        .hpoint = 0};
+  ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+}
+
+void app_main(void) {
+  print_chip_info();
+  ledc_init();
+
+  int remaining = 10;
+  int magnet_on = false;
+
+  for (;;) {
     vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+    remaining--;
+    printf("Switching to %s in %d seconds\n", magnet_on ? "off" : "on",
+           remaining);
+
+    if (remaining == 0) {
+      remaining = 10;
+      magnet_on = !magnet_on;
+
+      if (magnet_on) {
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY));
+      } else {
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 0));
+      }
+
+      ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
+    }
   }
-  printf("Restarting now.\n");
-  fflush(stdout);
-  esp_restart();
 }
